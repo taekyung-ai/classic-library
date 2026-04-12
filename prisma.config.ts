6a7a4e51b494ad564be87ba17281618980f134ff
テギョン <taekyung@gwagtaegyeong-ui-MacBookPro.local>
2026-04-12 13:59:01 +0200

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,5 @@ import path from 'node:path'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    url: 'postgres://f3460d9686bf8dd0054214f77051fb3677a3ed53f5e15cdb9d95c81f12d2ef50:sk_ieK0IdXMT5A2e-AKDdj8b@db.prisma.io:5432/postgres?sslmode=require',
-  },
 })
